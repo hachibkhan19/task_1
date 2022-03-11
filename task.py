19dd1812731e282a4cddfe505c12809d07c5d096
Hachib Khan <47360828+hachibkhan19@users.noreply.github.com>
2022-03-11 07:33:53 +0600

    calculating current age

---
+++ task.py
@@ -0,0 +1,27 @@
+def calculate_current_age(current_date, current_month, current_year,
+                          birth_date, birth_month, birth_year):
+
+    month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if birth_date > current_date:
+        current_month = current_month - 1
+        current_date = current_date + month[birth_month - 1]
+
+    if birth_month > current_month:
+        current_year = current_year - 1
+        current_month = current_month + 12
+
+    date = current_date - birth_date
+    month = current_month - birth_month
+    year = current_year - birth_year
+
+    print(f'Years:{year} months:{month} days:{date}')
+
+
+current_date, current_month, current_year = map(int, input('Please enter your current date in same line and between '
+                                                           'space: ').split('/'))
+birth_date, birth_month, birth_year = map(int, input(
+    'Please enter your birth date in same line and between space: ').split('/'))
+
+calculate_current_age(current_date, current_month, current_year,
+                      birth_date, birth_month, birth_year)
